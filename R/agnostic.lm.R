2972701eef5cff47e91ebd3cf3fe80f2d17699f1
vcoscrato <30760523+vcoscrato@users.noreply.github.com>
2017-08-26 12:49:41 -0300

    Add files via upload

--- R/agnostic.lm.R
+++ R/agnostic.lm.R
@@ -84,17 +84,3 @@ agnostic.lm <- function(formula, data, subset, weights, na.action, method = "qr"
     z$qr <- NULL
   z
 }
-
-print.agnostic.lm <- function (x, digits = max(3L, getOption("digits") - 3L), ...) 
-{
-  cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"), 
-      "\n\n", sep = "")
-  if (length(coef(x))) {
-    cat("Coefficients:\n")
-    print.default(format(coef(x), digits = digits), print.gap = 2L, 
-                  quote = FALSE)
-  }
-  else cat("No coefficients\n")
-  cat("\n")
-  invisible(x)
-}
